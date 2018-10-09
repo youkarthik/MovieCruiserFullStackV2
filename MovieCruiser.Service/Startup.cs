89d8e4576d2fbe2217961ca7db8b3d856e65ec10
Karthikeyan.Umapathy <karthikeyan.umapathy@cognizant.com>
2018-10-09 14:16:26 +0000

    configurationchanges

--- MovieCruiser.Service/Startup.cs
+++ MovieCruiser.Service/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MovieCruiser.Service.DataAccess;
 using MovieCruiser.Service.DB;
+using MovieCruiser.Service.Services;
 using Swashbuckle.AspNetCore.Swagger;
 
 namespace MovieCruiser.Service
@@ -28,7 +30,15 @@ namespace MovieCruiser.Service
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
             //services.AddDbContext<MoviesDbContext>(x => x.usesqlserver)
+
+            //injecting dependencies
+            services.AddScoped<IMoviesDbContext, MoviesDbContext>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+            services.AddScoped<IMovieService, MovieService>();
+
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Info { Title = "MovieCruiser API", Version = "v1", Description = "MovieCruiser API Description" });
@@ -53,7 +63,7 @@ namespace MovieCruiser.Service
                 c.SwaggerEndpoint("swagger/v1/swagger.json", "Movie Cruiser API");
             });
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
