6d0bacc008a15cddf6907d05c7a9f58496830c19
Karthikeyan.Umapathy <karthikeyan.umapathy@cognizant.com>
2018-11-08 09:49:20 +0000

    e2e testing added to angular app

--- MovieCruiser.UI/e2e/src/app.e2e-spec.ts
+++ MovieCruiser.UI/e2e/src/app.e2e-spec.ts
@@ -23,10 +23,11 @@ describe('movie-cruiser-angular-ui App', () => {
     expect(browser.getCurrentUrl()).toContain('/register');
   });
 
-  it('should be able to register user', () => {
+  it('should be able to register a new user', () => {
+    var uniq =(new Date()).getTime();
     browser.element(by.id('firstname')).sendKeys('test user 1');
     browser.element(by.id('lastname')).sendKeys('test user 1');
-    browser.element(by.id('userId')).sendKeys('usertest7');
+    browser.element(by.id('userId')).sendKeys('usertest1');
     browser.element(by.id('password')).sendKeys('password');
     browser.element(by.id('btnregsubmit')).click();
     expect(browser.getCurrentUrl()).toContain('/login');
@@ -45,20 +46,63 @@ describe('movie-cruiser-angular-ui App', () => {
     expect(browser.getCurrentUrl()).toContain('/movies/search');
     
 
-    // const count = element.all(by.css('.mat-card-title')).count();
-    // expect(count).toBe(20);
-    // for(let i = 0; i < 20; i++) {
-    //   expect(element.all(by.css('.mat-card-title')).get(i).getText()).toContain('best');
-    // }
+    const count = element.all(by.css('.mat-card-title')).count();
+    expect(count).toBe(20);
+    for(let i = 0; i < 20; i++) {
+      expect(element.all(by.css('.mat-card-title')).get(i).getText().then(x => x.toLowerCase())).toContain('best');
+    }
   });
 
-  // it('adding movie to watch list', async () => {
-  //   browser.driver.manage().window().maximize();
-  //   browser.driver.sleep(1000);
-  //   browser.element(by.css('.topRated')).click();
-  //   const count = element.all(by.css('.card')).count();
-  //   expect(count).toBe(20);
-  //   element.all(by.css('.card')).get(0).element(by.css('.addButton')).click();
-  //   //element(by.css('.addButton')).click();
-  // })
+  it('should be able to add a movie to the watchlist', async () => {
+    browser.driver.manage().window().maximize();
+    browser.driver.sleep(1000);
+    browser.element(by.css('.clsBtnTopRated')).click();
+    // const count = element.all(by.css('.card')).count();
+    // expect(count).toBe(20);
+    element.all(by.css('.movie-thumbnail')).get(0).element(by.css('.clsAddButton')).click();
+    browser.element(by.id('watchlistMovieComment')).sendKeys('samle test comment for the watch list movie');
+    browser.element(by.css('.clsSaveCommentsBtn')).click();
+    //element(by.css('.addButton')).click();
+  })
+
+  
+  it('should be able to view the movie added to watchlist', async () => {
+    browser.driver.manage().window().maximize();
+    browser.driver.sleep(1000);
+    browser.element(by.css('.clsBtnWatchlist')).click();
+    const count = element.all(by.css('.movie-thumbnail')).count();
+    expect(count).toBe(1);
+    //element(by.css('.addButton')).click();
+  })
+
+  it('updating comments in watchlist movie and verification', async () => {
+    var comment = 'updated sample test comment for the watch list movie';
+    browser.driver.manage().window().maximize();
+    browser.driver.sleep(100);
+    element.all(by.css('.movie-thumbnail')).get(0).element(by.css('.clsUpdateButton')).click();
+    browser.element(by.id('watchlistMovieComment')).clear();
+    browser.element(by.id('watchlistMovieComment')).sendKeys(comment);
+    browser.element(by.css('.clsSaveCommentsBtn')).click();
+    browser.driver.sleep(500);
+    expect(element.all(by.css('.movie-overview')).get(0).getText()).toContain(comment);
+    //element(by.css('.addButton')).click();
+  })
+
+  it('should be able to delete watchlist movie and verify', async () => {
+    browser.driver.manage().window().maximize();
+    browser.driver.sleep(100);
+    element.all(by.css('.movie-thumbnail')).get(0).element(by.css('.clsRemoveButton')).click();
+    browser.driver.sleep(500);
+    const count = element.all(by.css('.mat-card-title')).count();
+    expect(count).toBe(0);
+    //element(by.css('.clsBtnLogout')).click();
+  })
+
+  it ('should be able to log out of the application', async() => {
+    element(by.css('.clsBtnLogout')).click();
+    expect(browser.getCurrentUrl()).toContain('/login');
+    
+  })
+  
+  browser.driver.sleep(2000);
 });
